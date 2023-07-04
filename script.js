410041b21dfc53ad6d0ee1db0165099d8592f893
TN <naoufal.t@hotmail.com>
2023-07-04 14:02:01 +0200

    Update Code

--- script.js
+++ script.js
@@ -14,32 +14,35 @@ const fileName = document.querySelector(".fileName");
 
 const nmbImages = images.length;
 
-// Initialization
-fileName.textContent = getIndex()[1].replace(".jpg", "");
-
 // Events
 right_arrow.addEventListener("click", () => {
   let currentIndex = getIndex()[0];
-  const name = getIndex()[1].replace(".jpg", "");
   if (currentIndex === images.length - 1) currentIndex = 0;
   else currentIndex++;
-  image.src = "./img/" + images[currentIndex];
-  fileName.textContent = name;
+  const name = getIndex()[1].replace(".jpg", "");
+  loadImage(name, currentIndex);
 });
 left_arrow.addEventListener("click", () => {
   let currentIndex = getIndex()[0];
   const name = getIndex()[1].replace(".jpg", "");
   if (currentIndex === 0) currentIndex = images.length - 1;
   else currentIndex--;
-  image.src = "./img/" + images[currentIndex];
-  fileName.textContent = name;
+  loadImage(name, currentIndex);
 });
 
+// This function needs to be refactored
 function getIndex() {
   const url = image.src; //http://127.0.0.1:5500/Projects/slider/img/wp4676582-4k-pc-wallpapers.jpg
   const parts = url.split("/"); // removes / and give strings in between as an array
   const lastPart = parts[parts.length - 1]; // get the last string in the array above
   let currentIndex = images.indexOf(lastPart);
-  console.log(currentIndex);
+  // console.log("Old Index:", currentIndex); // Debug
   return [currentIndex, lastPart];
+}
+
+// Added new function
+function loadImage(name, index) {
+  // console.log("New Index:", index); // Debug
+  image.src = "./img/" + images[index];
+  fileName.textContent = name;
 }
